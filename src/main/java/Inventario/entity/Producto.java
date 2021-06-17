a04da1b080789c132dc86fc9f7f4640c26e44caa
DanielAristy <danielaristy22@gmail.com>
2021-06-17 11:15:04 -0500

    Comportamientos de la entidad Producto fueron implementados para la actualizacion de estos objetos de valor

--- src/main/java/Inventario/entity/Producto.java
+++ src/main/java/Inventario/entity/Producto.java
@@ -5,10 +5,31 @@ import Inventario.values.NombreProducto;
 import Inventario.values.ProductoId;
 import co.com.sofka.domain.generic.Entity;
 
+import java.util.Objects;
+
 public class Producto extends Entity<ProductoId> {
     private NombreProducto nombreProducto;
     private DescripcionProducto descripcionProducto;
     public Producto(ProductoId entityId, NombreProducto nombreProducto, DescripcionProducto descripcionProducto) {
         super(entityId);
+        this.nombreProducto = nombreProducto;
+        this.descripcionProducto = descripcionProducto;
+    }
+
+    //comportamientos
+    public void actulizarNombreProducto(NombreProducto nombreProducto){
+        this.nombreProducto = Objects.requireNonNull(nombreProducto);
+    }
+
+    public void actualizarDescripcionProducto(DescripcionProducto descripcionProducto){
+        this.descripcionProducto = Objects.requireNonNull(descripcionProducto);
+    }
+
+    public NombreProducto nombreProducto() {
+        return nombreProducto;
+    }
+
+    public DescripcionProducto descripcionProducto() {
+        return descripcionProducto;
     }
 }
