7aacb8a2ff2005a64ba709088eb7a72c426cacaa
DanielAristy <danielaristy22@gmail.com>
2021-06-17 11:11:58 -0500

    Actualizar los objetos de valor de la entidad TipoProducto

--- src/main/java/Inventario/entity/TipoProducto.java
+++ src/main/java/Inventario/entity/TipoProducto.java
@@ -5,10 +5,30 @@ import Inventario.values.NombreTipoProducto;
 import Inventario.values.TipoProductoId;
 import co.com.sofka.domain.generic.Entity;
 
+import java.util.Objects;
+
 public class TipoProducto extends Entity<TipoProductoId> {
     private NombreTipoProducto nombreTipoProducto;
     private Descuento descuento;
-    public TipoProducto(TipoProductoId entityId) {
+    public TipoProducto(TipoProductoId entityId, NombreTipoProducto nombreTipoProducto, Descuento descuento) {
         super(entityId);
+        this.nombreTipoProducto = nombreTipoProducto;
+        this.descuento = descuento;
+    }
+    //Comportamientos
+    public void actualizarNombreTipoProducto(NombreTipoProducto nombreTipoProducto){
+        this.nombreTipoProducto = Objects.requireNonNull(nombreTipoProducto);
+    }
+
+    public void actualizarDescuento(Descuento descuento){
+        this.descuento = Objects.requireNonNull(descuento);
+    }
+
+    public NombreTipoProducto nombreTipoProducto() {
+        return nombreTipoProducto;
+    }
+
+    public Descuento descuento() {
+        return descuento;
     }
 }
