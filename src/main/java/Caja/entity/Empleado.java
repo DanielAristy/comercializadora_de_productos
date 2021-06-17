8afc81600d1f038c0265b72eb67fc14b64457c9d
DanielAristy <danielaristy22@gmail.com>
2021-06-17 11:06:08 -0500

    Comportamiento de la entidad Empleado y se actulizan los objetos de esta entidad

--- src/main/java/Caja/entity/Empleado.java
+++ src/main/java/Caja/entity/Empleado.java
@@ -6,11 +6,36 @@ import Caja.values.Estado;
 import Caja.values.NombreEmpleado;
 import co.com.sofka.domain.generic.Entity;
 
+import java.util.Objects;
+
 public class Empleado extends Entity<EmpleadoId> {
     private CajaId cajaId;
     private NombreEmpleado nombreEmpleado;
     private Estado estado;
     public Empleado(EmpleadoId entityId, CajaId cajaId, NombreEmpleado nombreEmpleado, Estado estado) {
         super(entityId);
+        this.cajaId = cajaId;
+        this.nombreEmpleado = nombreEmpleado;
+        this.estado = estado;
+    }
+    //Comportamientos
+    public void actualizarNombreEmpleado(NombreEmpleado nombreEmpleado){
+        this.nombreEmpleado = Objects.requireNonNull(nombreEmpleado);
+    }
+
+    public void actualizarEstado(Estado estado){
+        this.estado = Objects.requireNonNull(estado);
+    }
+
+    public CajaId cajaId() {
+        return cajaId;
+    }
+
+    public NombreEmpleado nombreEmpleado() {
+        return nombreEmpleado;
+    }
+
+    public Estado estado() {
+        return estado;
     }
 }
