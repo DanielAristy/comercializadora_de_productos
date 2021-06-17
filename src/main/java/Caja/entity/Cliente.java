593159f0b98fede6a4e18120681fd8ecbf71a0f0
DanielAristy <danielaristy22@gmail.com>
2021-06-17 11:00:53 -0500

    Comportamiento agregados de la entidad Cliente en el cual se actulizan los objetos de esta entidad

--- src/main/java/Caja/entity/Cliente.java
+++ src/main/java/Caja/entity/Cliente.java
@@ -1,10 +1,35 @@
 package Caja.entity;
 
 import Caja.values.ClienteId;
+import Caja.values.Direccion;
+import Caja.values.NombreCliente;
 import co.com.sofka.domain.generic.Entity;
 
+import java.util.Objects;
+
 public class Cliente extends Entity<ClienteId> {
-    public Cliente(ClienteId entityId) {
+    private NombreCliente nombreCliente;
+    private Direccion direccion;
+    public Cliente(ClienteId entityId, NombreCliente nombreCliente, Direccion direccion) {
         super(entityId);
+        this.nombreCliente = nombreCliente;
+        this.direccion = direccion;
+    }
+    //Comportamientos de clase
+
+    public void actualizarNombreCliente(NombreCliente nombreCliente){
+        this.nombreCliente = Objects.requireNonNull(nombreCliente);
+    }
+
+    public void actualizarDireccion(Direccion direccion){
+        this.direccion = Objects.requireNonNull(direccion);
+    }
+
+    public NombreCliente nombreCliente() {
+        return nombreCliente;
+    }
+
+    public Direccion direccion() {
+        return direccion;
     }
 }
---
+++ src/main/java/Caja/values/NombreCliente.java
@@ -0,0 +1,10 @@
+package Caja.values;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+public class NombreCliente implements ValueObject<String> {
+    @Override
+    public String value() {
+        return null;
+    }
+}
